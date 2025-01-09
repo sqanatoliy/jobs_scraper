e7d4abd83e8156fa0d257c2b9ca966b0da608d14
antolii <a.olesh@e-docs.ua>
2025-01-09 14:50:02 +0200

    add comments

--- run_scraper.py
+++ run_scraper.py
@@ -1,4 +1,3 @@
-
 """
 This script checks for new job postings on different websites 
 and sends them to a specified Telegram chat.
@@ -7,17 +6,13 @@ Usage:
 1. Ensure that the .env file contains the TELEGRAM_TOKEN and CHAT_ID.
 2. Run the script to check for new job postings and send them to the specified Telegram chat.
 """
-import os
-from dotenv import load_dotenv
 
 from config.scraper_config import DouScraperConfig, GlobalLogicScraperConfig
 from src.gb_lg_job_scraper import GlobalLogicJobScraper
 from src.dou_job_scraper import DouJobScraper
 from config.settings import TELEGRAM_TOKEN, CHAT_ID, NO_EXP_TELEGRAM_TOKEN, NO_EXP_CHAT_ID, DB_PATH
 
-load_dotenv()
-
-
+# GLOBAL LOGIC CONFIGURATIONS ==========================================================
 # Configuration for checking new jobs for experience level 0-1 years on GlobalLogic
 gl_lg_python_0_1 = GlobalLogicScraperConfig(
     telegram_token=TELEGRAM_TOKEN,
@@ -27,7 +22,6 @@ gl_lg_python_0_1 = GlobalLogicScraperConfig(
     experience="0-1+years",
     locations="ukraine",
 )
-
 # Configuration for checking new jobs for experience level 1-3 years on GlobalLogic
 gl_lg_python_1_3 = GlobalLogicScraperConfig(
     telegram_token=TELEGRAM_TOKEN,
@@ -37,7 +31,9 @@ gl_lg_python_1_3 = GlobalLogicScraperConfig(
     experience="1-3+years",
     locations="ukraine",
 )
+# END GLOBAL LOGIC CONFIGURATIONS ======================================================
 
+# DOU CONFIGURATIONS ==================================================================
 # Configuration for checking new jobs for experience level 0-1 years on DOU
 dou_python_config_0_1 = DouScraperConfig(
     db_path=DB_PATH,
@@ -74,14 +70,17 @@ dou_support_remote_config_0_1 = DouScraperConfig(
     experience="0-1",
     remote=True,
 )
+# END DOU CONFIGURATIONS ============================================================
 
-#####################################################################################################
+# GLOBAL LOGIC JOB SCRAPING ==========================================================
 # Check new Python jobs for experience level 0-1 years on GlobalLogic
 GlobalLogicJobScraper(gl_lg_python_0_1).check_and_add_jobs()
 
 # Check new Python jobs for experience level 1-3 years on GlobalLogic
 GlobalLogicJobScraper(gl_lg_python_1_3).check_and_add_jobs()
+# END GLOBAL LOGIC JOB SCRAPING ======================================================
 
+# DOU JOB SCRAPING ===================================================================
 # Check new Python jobs for experience level 0-1 years on DOU
 DouJobScraper(dou_python_config_0_1).check_and_add_jobs()
 
@@ -94,3 +93,4 @@ DouJobScraper(dou_no_exp_remote_config).check_and_add_jobs()
 
 # Check new Support remote jobs for experience level 0-1 years on DOU
 DouJobScraper(dou_support_remote_config_0_1).check_and_add_jobs()
+# END DOU JOB SCRAPING ===============================================================
