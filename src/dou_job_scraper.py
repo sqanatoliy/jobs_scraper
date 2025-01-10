ae279de7dfa49af8b4dc5bf5853f5e541d06c29c
antolii <a.olesh@e-docs.ua>
2025-01-10 15:01:57 +0200

    upd log msg

--- src/dou_job_scraper.py
+++ src/dou_job_scraper.py
@@ -186,7 +186,7 @@ class DouJobScraper:
                     logging.error("An unexpected error occurred: %s", e)
                     conn.rollback()
         if not new_jobs:
-            logging.info("No new jobs found at DOU.")
+            logging.info(f"No new jobs found at DOU {job.category} category.")
         return new_jobs
 
     def _create_telegram_message(self, job: DouJob) -> str:
